6635218b307dacd52ae1ede37cb6c377a32e68af
habib-7861 <120159688+habib-7861@users.noreply.github.com>
2023-01-28 14:59:28 +0500

    Add files via upload

--- Task4.cpp
+++ Task4.cpp
@@ -1,42 +1,41 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
-int discount(string day,string month,float price)
-{
-    float discounts;
-    if(day == "sunday" && (month == "october"|| month =="march" || month == "august"))
-    {
-        float disc = price - (price*0.10);
-        discounts = disc;
-    }
-    else if(day == "monday" && (month == "november"|| month =="december"))
-    {
-        float disc = price - (price*0.05);
-        discounts = disc;
-    }
-    else 
-    {
-        discounts = price;
-    }
-    return discounts;
-
-
-
-}
+float project(float n_hours,float days,float workers);
 main()
 {
     system("cls");
-    while(true)
+    float n_hours,days,workers;
+    int percentage_days = 10;
+    int hours_per_day = 8;
+    int extra_hours = 2;
+    cout <<"Enter needed hours: ";
+    cin >>n_hours;
+    cout <<"Enter days that firm has: ";
+    cin >>days;
+    cout <<"Enter number of workers: ";
+    cin >>workers;
+    float result = project(n_hours,days,workers); 
+    if(result > n_hours)
     {
-        string day,month;
-        int price;
-        float payable;
-        cout <<"Enter day: ";
-        cin>>day;
-        cout <<"Enter month: ";
-        cin>>month;
-        cout <<"Enter price: ";
-        cin>>price;
-        float result = discount(day,month,price);
-        cout <<result<<endl;
+        float Hs_left = result - n_hours;
+        cout <<"Yes! "<<Hs_left<<" "<<"hours left.";
     }
+    if(result < n_hours)
+    {
+        float Hs_needed = n_hours - result;
+        cout <<"Not enough time! "<<Hs_needed<<" "<<"hours needed.";
+    }
+}
+float project(float n_hours,float days,float workers)
+{
+    int percentage_days = 10;
+    int hours_per_day = 8;
+    int extra_hours = 2;
+    float days_per = days*percentage_days/100;
+    float days_left = days - days_per;
+    float day_H = hours_per_day + extra_hours;
+    float days_H = days_left * day_H;
+    float total_H = workers*days_H;
+    return total_H;
 }
